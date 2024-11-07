74ba90cad817ee253087817992a88f84075272a5
geebson <lukeg23662@gmail.com>
2024-11-07 14:57:00 -0500

    Started minimax implementation

--- castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
+++ castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
@@ -9,7 +9,15 @@ class CastleCrashers_ChessPlayer(ChessPlayer):
         super().__init__(board, color)
 
     def get_move(self, your_remaining_time, opp_remaining_time, prog_stuff):
-        #check which color we are
+        #basic minimax implementation
+        #adjust parameters based on the piece color
+        if(self.color=='white'):
+            best_score = -10000
+            isMazimizing = True
+        else:
+            best_score = 10000
+            isMazimizing = False
+
         return random.choice(
             self.board.get_all_available_legal_moves(self.color))
 
