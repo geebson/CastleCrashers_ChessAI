e0cfc6445050a7918113b8043400f03a5b62f4a7
geebson <lukeg23662@gmail.com>
2024-11-07 14:45:53 -0500

    updated CastleCrashers_ChessPlayer class definition

--- castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
+++ castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
@@ -3,12 +3,13 @@ import random
 
 from chess_player import ChessPlayer
 
-class Random_ChessPlayer(ChessPlayer):
+class CastleCrashers_ChessPlayer(ChessPlayer):
 
     def __init__(self, board, color):
         super().__init__(board, color)
 
     def get_move(self, your_remaining_time, opp_remaining_time, prog_stuff):
+        #check which color we are
         return random.choice(
             self.board.get_all_available_legal_moves(self.color))
 
