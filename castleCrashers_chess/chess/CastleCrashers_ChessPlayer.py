e8ef6ec4dbeaf99d7d20f1971dc538873354adef
jbtsibugy <51725213+jbtsibugy@users.noreply.github.com>
2024-11-08 15:11:04 -0500

    Add files via upload
    Minimax Working just need to add evaluation function

--- castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
+++ castleCrashers_chess/chess/CastleCrashers_ChessPlayer.py
@@ -1,28 +1,69 @@
-
 import random
-
-from chess_player import ChessPlayer
 from copy import deepcopy
 
+from chess_player import ChessPlayer
+
 class CastleCrashers_ChessPlayer(ChessPlayer):
-
     def __init__(self, board, color):
-        super().__init__(board, color)
-
-    def get_move(self, your_remaining_time, opp_remaining_time, prog_stuff):
-        #basic minimax implementation
-        #adjust parameters based on the piece color
-        if(self.color=='white'):
-            best_score = -10000
-            isMazimizing = True
+        super().__init__(board,color)
+    
+        self.maxDepth = 1
+    def evalFunction(self,board):
+        return 0
+    
+    def get_move(self,your_remaining_time, opp_remaining_time, prog_stuff):
+        #return random.choice(self.board.get_all_available_legal_moves(self.color))
+        boardcopy = deepcopy(self.board)
+        legal_moves = boardcopy.get_all_available_legal_moves(self.color)
+        
+        if(self.color == 'white'):
+      
+            bestscore = -1000000000000
+            for x in legal_moves:
+                boardcopy = deepcopy(self.board)
+                boardcopy.make_move(x[0],x[1])
+                score = self.minimax(boardcopy, 0,True)
+                if (score > bestscore):
+                    bestscore = score
+                    bestMove = x
+            return bestMove            
+        elif (self.color == 'black'):
+            bestscore = 1000000000000
+            for x in legal_moves:
+                boardcopy = deepcopy(self.board)
+                boardcopy.make_move(x[0],x[1])
+                score = self.minimax(boardcopy, 0,False)
+                if (score < bestscore):
+                    bestscore = score
+                    bestMove = x
+            return bestMove            
+                
+    def minimax(self, board, depth, isMaximizing):
+        legal_moves = board.get_all_available_legal_moves(self.color)
+        if (board.is_king_in_checkmate("white")):
+            score = -1000000
+        elif(board.is_king_in_checkmate("black")):
+            score = 1000000
+        
+        if (depth == self.maxDepth):
+            return self.evalFunction(board)
+        
+        if(isMaximizing):
+            bestScore = -1000000000000  
+            for x in legal_moves:
+                boardcopy = deepcopy(board)
+                boardcopy.make_move(x[0],x[1])
+                score = self.minimax(boardcopy, depth +1 , False)
+                bestScore = max(score, bestScore)
+            return bestScore           
         else:
-            best_score = 10000
-            isMazimizing = False
-
-        #setup board and moves
-        board_copy = deepcopy(self.board)
-        possible_moves = board_copy.get_all_available_legal_moves(self.color)
-        print(possible_moves)
-        return random.choice(
-            self.board.get_all_available_legal_moves(self.color))
-
+            bestScore = 1000000000000
+            for x in legal_moves:
+                boardcopy = deepcopy(board)
+                boardcopy.make_move(x[0],x[1])
+                score = self.minimax(boardcopy, depth +1 , True)
+                bestScore = min(score, bestScore)
+            return bestScore      
+            
+            
+            
